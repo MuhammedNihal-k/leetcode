f1e22507531e8ef46f120fb9ec38fa1febc8cd11
MuhammedNihal-k <nihalxmohd@gmail.com>
2026-07-04 14:08:44 +0530

    Time: 231 ms (41.63%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1734-bank-account-summary-ii/bank-account-summary-ii.sql
@@ -0,0 +1,2 @@
+-- Write your PostgreSQL query statement below
+SELECT name ,SUM(amount)AS balance FROM Users LEFT JOIN Transactions ON Users. account = Transactions.account GROUP BY name HAVING SUM(amount) >10000;
