6c563cd8cdfa48f8f43f2bdc49fca0ef3a02abc0
MuhammedNihal-k <nihalxmohd@gmail.com>
2026-09-11 12:14:07 +0530

    Time: 194 ms (39.25%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,19 @@
+-- Write your PostgreSQL query statement below
+WITH friends AS (
+    SELECT requester_id AS id
+    FROM RequestAccepted
+
+    UNION ALL
+
+    SELECT accepter_id AS id
+    FROM RequestAccepted
+),
+friend_count AS (
+    SELECT id, COUNT(*) AS num
+    FROM friends
+    GROUP BY id
+)
+SELECT id, num
+FROM friend_count
+ORDER BY num DESC
+LIMIT 1;
